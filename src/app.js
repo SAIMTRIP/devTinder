d9bfc1f90bb4f7572d72d3a822b6eb344a0bab95
Somesh Tripathi <somesh.tripathi007.st@gmail.com>
2024-09-30 23:58:50 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -15,6 +15,19 @@ app.get('/',(req, res, next)=>{
     res.send({firstName: "somesh", lastName: "tripathi"})
  })
 
+ app.get('/user', (req, res, next)=>{
+    console.log("Handling route user ");
+    next();
+    // res.send("Response 1")
+    next();
+ }, [(req, res, next)=>{
+    console.log("Response 2");
+    next();
+ }, (req, res, next)=>{
+    console.log("Response 3");
+    res.send("Response 3");
+ }])
+
 app.post('/hello/:userId/:userName/:password',(req, res, next)=>{
     console.log(req.params);
     res.send("Hello from server");
